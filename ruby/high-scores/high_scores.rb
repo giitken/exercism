6c5ae26262756e27e7cbc61182dfef3f466701d2
ken <kirie115@gmail.com>
2019-03-06 16:44:35 +0900

    update gitignore

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,24 @@
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def scores
+    @scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.max(3).sort.reverse
+  end
+
+end
