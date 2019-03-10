578dab0b6330427947eee7bc754704d66fc441cf
ken <kirie115@gmail.com>
2019-03-10 10:07:25 +0900

    [modifiled]インスタンス生成時に値をArrayに加工するよう修正

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,13 +1,12 @@
 class Phrase
 
   def initialize(sentence)
-    @sentence = sentence
+    @sentence = sentence.scan(/\w+'t|\w+/).map(&:downcase)
   end
 
   def word_count
-    each_word = @sentence.scan(/\w+'t|\w+/).map(&:downcase)
-    each_word.uniq.each_with_object({}) {|word, hash|
-      hash.store(word, each_word.count(word))
-    }
+    @sentence.uniq.each_with_object({}) do |word, hash|
+      hash[word] = @sentence.count(word)
+    end
   end
 end
