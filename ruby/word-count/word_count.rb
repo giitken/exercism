9935cf9972c3003518495ee357fb59d1abea25ea
ken <kirie115@gmail.com>
2019-03-12 10:19:15 +0900

    refactored

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,12 +1,23 @@
 class Phrase
+  attr_reader :sentence, :tallied_nums
 
   def initialize(sentence)
-    @sentence = sentence.scan(/\w+'t|\w+/).map(&:downcase)
+    @sentence = sentence
+    @tallied_nums = tally_nums
   end
 
   def word_count
-    @sentence.uniq.each_with_object({}) do |word, hash|
-      hash[word] = @sentence.count(word)
-    end
+    tallied_nums
   end
+
+  private
+
+  def each_word
+    sentence.scan(/\w+'t|\w+/).map(&:downcase)
+  end
+
+  def tally_nums
+    each_word.uniq.each_with_object({}) {|word, hash| hash[word] = each_word.count(word)}
+  end
+
 end
