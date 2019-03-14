cc3fda826d1be3d9a04cbbeeee86ab808b9f61fe
ken <kirie115@gmail.com>
2019-03-14 10:04:33 +0900

    refactored

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,6 @@
 class Hamming
   def self.compute(orig, copy)
     raise ArgumentError if orig.size != copy.size
-    diff_sum = 0
-    orig.size.times{|idx| diff_sum += 1 if orig[idx] != copy[idx]}
-    diff_sum
+    orig.size.times.count{|idx| orig[idx] != copy[idx]}
   end
 end
