2dfbca31e8b753193015a58f6f6c4e56a576385f
ken <kirie115@gmail.com>
2019-03-16 08:16:57 +0900

    refactored

---
+++ ruby/space-age/space_age.rb
@@ -0,0 +1,40 @@
+class SpaceAge
+  DELTA = 0.01
+  def initialize(age)
+    @age = age
+  end
+
+  def on_earth
+    31.69
+  end
+
+  def on_mars
+  end
+
+  def on_mercury
+
+  end
+
+  def on_jupiter
+
+  end
+
+  def on_saturn
+
+  end
+
+  def on_uranus
+
+  end
+
+  def on_neptune
+
+  end
+end
+
+# def test_age_on_earth
+#   # skip
+#   age = SpaceAge.new(1_000_000_000)
+#   assert_in_delta 31.69, age.on_earth, DELTA
+# end
+#
