93c5a395b8c2fb9be0bdf9cc66cb798c5645b9ec
ken <kirie115@gmail.com>
2019-03-16 08:18:02 +0900

    refactored

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,12 +1,12 @@
 class Raindrops
-  FACTORS_TABLE = {
+  FACTORS = {
       3 => 'Pling',
       5 => 'Plang',
       7 => 'Plong',
   }
 
   def self.convert(num)
-    sounds = FACTORS_TABLE.map {|k, v| v if (num % k).zero?}.join
+    sounds = FACTORS.map {|k, v| v if (num % k).zero?}.join
     sounds.empty? ? num.to_s : sounds
   end
 end
