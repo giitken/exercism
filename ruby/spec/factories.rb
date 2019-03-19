65c73a219e5e9ffe467b36284a261137a4e42638
ken <kirie115@gmail.com>
2019-03-19 17:16:34 +0900

    RSpec related files

---
+++ ruby/spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :staff do
+    staff_last_name  "名字"
+    staff_first_name "名前"
+    staff_short_name "名字"
+    position_div "2"
+    create_id "TEST01"
+    update_id "TEST01"
+    lock_version "1"
+  end
+end
