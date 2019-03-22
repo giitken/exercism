ca725537551a0d7812d11ab37fd06d312b0be93e
ken <kirie115@gmail.com>
2019-03-22 09:26:56 +0900

    refactored

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,29 +1,25 @@
 class Triangle
-  attr_reader :sides
 
   def initialize(sides)
     @sides = sides
   end
 
   def equilateral?
-    sides.uniq.size == 1 && valid?
+    @sides.uniq.size == 1 && valid?
   end
 
   def isosceles?
-    sides.uniq.size <= 2 && valid?
+    @sides.uniq.size <= 2 && valid?
   end
 
   def scalene?
-    sides.uniq.size == 3 && valid?
+    @sides.uniq.size == 3 && valid?
   end
 
   private
 
   def valid?
-    indices = [0, 1, 2]
-    3.times.all? do
-      indices.rotate!
-      (sides[indices[0]] + sides[indices[1]]) > sides[indices[2]]
-    end
+    a, b, c = @sides.sort
+    a > 0 && c <= a + b
   end
 end
