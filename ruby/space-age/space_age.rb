e732001cea0486299909d2cfa647d898a36d99a8
ken3ypa <kirie115@gmail.com>
2019-07-13 11:38:43 +0900

    Refactored with difine_method

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,47 +1,27 @@
 class SpaceAge
-  EARTH_YEAR = 31_557_600.0
-
   attr_reader :seconds
+
+  EARTH_YEAR_IN_SECONDS = (60 * 60 * 24 * 365.25) # sum is 315_57_600
+
+  PLANETS = {
+      earth:   1.0,
+      mercury: 0.2408467,
+      venus:   0.61519726,
+      mars:    1.8808158,
+      jupiter: 11.862615,
+      saturn:  29.447498,
+      uranus:  84.016846,
+      neptune: 164.79132
+  }.map(&:freeze).to_h.freeze
 
   def initialize(seconds)
     @seconds = seconds
   end
 
-  def on_earth
-    to_years
-  end
-
-  def on_mercury
-    to_years(0.2408467)
-  end
-
-  def on_venus
-    to_years(0.61519726)
-  end
-
-  def on_mars
-    to_years(1.8808158)
-  end
-
-  def on_jupiter
-    to_years(11.862615)
-  end
-
-  def on_saturn
-    to_years(29.447498)
-  end
-
-  def on_uranus
-    to_years(84.016846)
-  end
-
-  def on_neptune
-    to_years(164.79132)
-  end
-
-  private
-
-  def to_years(orbital_ratio = 1)
-    (seconds / (EARTH_YEAR * orbital_ratio)).round(2)
+  PLANETS.each do |planet_name, orbital_period|
+    define_method "on_#{planet_name}" do
+      seconds / (EARTH_YEAR_IN_SECONDS * orbital_period)
+    end
   end
 end
+
