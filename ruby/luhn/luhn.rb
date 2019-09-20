b6b2c4e5b681ca01fafb0af28b5a087778ffd5df
簗瀬健介 <kirie115@gmail.com>
2019-09-21 00:34:20 +0900

    クラスメソッドを呼び出した際、インスタンスメソッド経由で処理をするよう変更した

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,31 +1,39 @@
 class Luhn
-  class << self
-    def valid?(digits)
-      reversed_digits = digits.reverse.gsub(/\s/, '')
-      return false unless reversed_digits.match?(TWO_OR_MORE_DIGITS)
+  attr_reader :digits
 
-      adjusted_digits = reversed_digits.chars.each_with_index.inject([]) { |ary, (digit, idx)| ary << adjust_num(digit, idx) }
-      verify_digits(adjusted_digits)
+  def initialize(digits)
+    @digits = digits
+  end
+
+  def valid?
+    return false unless normalized_digits.match?(TWO_OR_MORE_DIGITS)
+
+    (sum_total % 10).zero? && is_more_than_double?
+  end
+
+  def self.valid?(digits)
+    new(digits).valid?
+  end
+
+  private
+
+  TWO_OR_MORE_DIGITS = /\A\d{2,}\z/
+
+  def normalized_digits
+    @normalized_digits ||= digits.reverse.gsub(/\s/, '')
+  end
+
+  def sum_total
+    @total ||= normalized_digits.chars.map(&:to_i).each_with_index.inject(0) do |total, (digit, idx)|
+      total += idx.even? ? digit : doubled_num(digit)
     end
+  end
 
-    private
+  def doubled_num(digit)
+    (num = digit.to_i * 2) > 9 ? num - 9 : num
+  end
 
-    TWO_OR_MORE_DIGITS = /\A\d{2,}\z/
-
-    def adjust_num(digit, idx)
-      idx.even? ? digit.to_i : doubled_num(digit)
-    end
-
-    def doubled_num(digit)
-      (num = digit.to_i * 2) > 9 ? num - 9 : num
-    end
-
-    def verify_digits(adjusted_digits)
-      (adjusted_digits.sum % 10).zero? && is_more_than_double_or_nonzero?(adjusted_digits)
-    end
-
-    def is_more_than_double_or_nonzero?(adjusted_digits)
-      adjusted_digits.length > 1 || adjusted_digits.sum.nonzero?
-    end
+  def is_more_than_double?
+    digits.length > 1
   end
 end
